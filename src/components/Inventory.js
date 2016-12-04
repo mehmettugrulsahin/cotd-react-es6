f34f3dda7f1280aadc9381a15737fda2c6970ad5
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2016-12-04 12:35:43 +0100

    Updates inventory state

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -2,15 +2,51 @@ import React from 'react';
 import AddFishForm from './AddFishForm';
 
 class Inventory extends React.Component {
-    render() {
-        return (
-            <div>
-                <h2>Inventory</h2>
-                <AddFishForm addFish={this.props.addFish} />
-                <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
-            </div>
-        )
-    }
+  constructor() {
+    super();
+    this.renderInventory = this.renderInventory.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e, key) {
+    const fish = this.props.fishes[key];
+    const updatedFish = {
+      ...fish, 
+      [e.target.name]: e.target.value
+    };
+    this.props.updateFish(key, updatedFish);
+  }
+
+  renderInventory(key) {
+    const fish = this.props.fishes[key];
+
+    return (
+      <div className="fish-edit" key={key}>
+        <input type="text" name="name" value={fish.name} onChange={(e) => this.handleChange(e, key)} placeholder="Fish name" />
+        <input type="text" name="price" value={fish.price} onChange={(e) => this.handleChange(e, key)} placeholder="Fish price" />
+        <select type="text" name="status" value={fish.status} onChange={(e) => this.handleChange(e, key)} placeholder="Fish status">
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out!</option>        
+        </select>
+        <textarea type="text" name="desc" value={fish.desc} onChange={(e) => this.handleChange(e, key)} placeholder="Fish desc">
+        </textarea>
+        <input type="text" name="image" value={fish.image} onChange={(e) => this.handleChange(e, key)} placeholder="Fish image" />
+      </div>
+    )
+  }
+
+  render() {
+    const {fishes} = this.props;
+
+    return (
+      <div>
+        <h2>Inventory</h2>
+        {Object.keys(fishes).map(this.renderInventory)}
+        <AddFishForm addFish={this.props.addFish} />
+        <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
+      </div>
+    )
+  }
 }
 
 export default Inventory;
